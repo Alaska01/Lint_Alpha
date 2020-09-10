42e1651fd1fd8cbc602350d5aafe9a0872385c4a
Alaska01 <aadaniel108@gmail.com>
2020-09-10 16:03:00 +0100

    create the indentation method

--- lib/styling_rules.rb
+++ lib/styling_rules.rb
@@ -8,6 +8,18 @@ class StylingRules
     @file = file
   end
 
+  def indentation(file)
+    file.each_with_index do |line, index|
+       if line.include? (";")
+         arr = line.scan(/./)
+         if arr[0] != " " || arr[1] != " " || arr[2] == " "
+           @error << "LINE #{index +1}: There is an indentation error, TWO spaces please"
+         end
+       end
+     end
+      @error
+   end
+
 
 
 end
